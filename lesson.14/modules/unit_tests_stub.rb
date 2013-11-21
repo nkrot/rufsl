14bcb997f5901e93534eaadaf00a040b831f5bca
Nikolai Krot <talpus@gmail.com>
2013-11-21 20:48:59 +0300

    updated

---
+++ lesson.14/modules/unit_tests_stub.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'test/unit'
+#require '' # your class or module
+
+# find the list of assertions here
+#http://en.wikibooks.org/wiki/Ruby_Programming/Unit_testing
+#http://ruby-doc.org/stdlib-2.0.0/libdoc/test/unit/rdoc/Test/Unit/Assertions.html#method-i-assert
+
+# TODO: replace XXX with something meaningful
+class TestXXX < Test::Unit::TestCase
+
+  def test_1
+    ...
+  end
+
+  def test_2
+    ...
+  end
+
+end
+
