cf27516cc00da1a895dc0831d10941aff2015edd
Nikolai Krot <talpus@gmail.com>
2014-02-17 01:55:08 +0300

    string splitter and demo script ready

--- other_tasks/string_splitter/string_splitter.rb
+++ other_tasks/string_splitter/string_splitter.rb
@@ -31,6 +31,17 @@ class StringSplitter
     @allow_unknown_single_letter = false
   end
 
+  # why double pass algorithm is important:
+  #sent = 'стоиктебекрепкийаромат'
+  # if two passes are applied (@allow_unknown_single_letter is used) then splittings are
+  #  стоик тебе крепкий аромат
+  #  сто и ктебе крепкий аромат
+  #  сто и к тебе крепкий аромат
+  #  с то и ктебе крепкий аромат
+  #  с то и к тебе крепкий аромат
+  # if all is done in a single pass, then the output is only
+  #  стоик тебе крепкий аромат
+  #  сто и ктебе крепкий аромат
   def process str
     ready?
     reset
@@ -44,8 +55,6 @@ class StringSplitter
     end
 
     if @do_postprocessing
-      # TODO: convert to array of Word object, where each word has a property: known_to_dictionary
-      # optionally, join single letters into a word
       postprocess
     end
 
@@ -63,16 +72,13 @@ class StringSplitter
       @splittings
     end
   end
+  alias results result
 
   private
 
-  def convert_to_words str
-    str.split
-  end
-
   def postprocess
     @raw_splittings.each do |str|
-      @splittings << convert_to_words(str)
+      @splittings << convert_to_splitting(str)
     end
   end
 
@@ -90,7 +96,7 @@ class StringSplitter
       next  if len > word.length
 
       head = word[0,len]
-      is_known_word = word?(head)
+      is_known_word = word?(head) || punctuation?(head)
 
       # additional condition (!is_known_word) is correct and allows generating
       # *all* splittings, which slows down the execution considerably.
@@ -130,7 +136,11 @@ class StringSplitter
   end
 
   def word? wd
-    @dict.include?(wd) || !!wd.match(/^([[:punct:]]|[\da-zA-Z]+)$/)
+    @dict.has?(wd) || !!wd.match(/^([\da-zA-Z]+)$/)
+  end
+
+  def punctuation? wd
+    !!wd.match(/^[[:punct:]]$/)
   end
 
   def ready?
@@ -145,4 +155,88 @@ class StringSplitter
     end
   end
 
+  def convert_to_splitting str
+    words = Splitting.new
+    word = nil
+
+    str.split.each do |w|
+      if w.length == 1 && !punctuation?(w)
+#        puts "single word: '#{w}'"
+        if word
+          word.append w
+        else
+          word = Word.new(w)
+          words << word
+        end
+      else
+        words << Word.new(w)
+        word = nil
+      end
+    end
+    
+    offset = 0
+    words.each do |word|
+      word.offset = offset
+      word.known = word?(word.text) || punctuation?(word.text)
+      offset += word.length
+    end
+    
+    words
+  end
+
+  ##################################################################
+
+  class Splitting < Array
+    def word_at pos
+      @char2words ||= map_char_positions_to_words
+      @char2words[pos]
+    end
+
+    private
+    def map_char_positions_to_words
+      @char2words = {}
+      self.each do |word|
+        s,e = word.offsets
+        s.upto(e) { |pos| @char2words[pos] = word }
+      end
+      @char2words
+    end
+  end
+
+  ##################################################################
+
+  class Word
+    attr_accessor :text, :known, :offset
+
+    def initialize text=nil
+      @text = text || ''
+      @known = false
+      @offset = -1
+    end
+
+    def append text
+      @text << text
+    end
+
+    def length
+      @text.length
+    end
+    alias size length
+
+    def unknown?
+      ! known?
+    end
+
+    def known?
+      @known
+    end
+
+    def end_offset
+      offset + length - 1
+    end
+
+    def offsets
+      [offset, end_offset]
+    end
+  end
 end
